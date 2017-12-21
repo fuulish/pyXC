119ebd089ae1b200bf53a069cc6d48e3aac1057b
Frank Uhlig <uhlig.frank@gmail.com>
2017-12-21 15:25:08 +0100

    adding some more tests and preliminary option to directly pass density

--- pyxc/__init__.py
+++ pyxc/__init__.py
@@ -11,13 +11,17 @@ class XC(object):
         self.func_id = np.array(func_id, dtype='int')
         self.c = deepcopy(c)
 
-    def get_exc(self, sgm=None):
-        dx = self.c.box_dim / self.c.data.shape
-        grd = np.gradient(self.c.data, dx[0], dx[1], dx[2])
+    def get_exc(self, rho=None, sgm=None):
 
-        dns = self.c.data.flatten().astype('float64')
+        if rho is None:
+            dns = self.c.data.flatten().astype('float64')
+        else:
+            dns = rho
 
         if sgm is None: #and gga and higher...
+            dx = self.c.box_dim / self.c.data.shape
+            grd = np.gradient(self.c.data, dx[0], dx[1], dx[2])
+
             sgm = grd[0]*grd[0] + grd[1]*grd[1] + grd[2]*grd[2]
             sgm = sgm.flatten().astype('float64')
 
--- pyxc/test.py
+++ pyxc/test.py
@@ -14,5 +14,23 @@ class TestXC(unittest.TestCase):
 
         self.assertAlmostEqual(EXC, -8.2939935007, places=5)
 
+    def test_cp2k_hydrogen(self):
+        c = cube.Cube('data/cp2k_rtp_0_0_1-ELECTRON_DENSITY-1.cube')
+        xc = XC(c)
+        exc = xc.get_exc()
+        EXC = (exc * c.data.flatten()).sum() * c.dV
+
+        self.assertAlmostEqual(EXC, -0.58913097576700, places=3)
+
+    def test_simple_libxc(self):
+        c = cube.Cube('data/simple.cube')
+        xc = XC(c, func_id=[1,])
+
+        exc = xc.get_exc(sgm=np.array([0.2, 0.3, 0.4, 0.5, 0.6]))
+
+        ref = np.array([ -0.342809, -0.431912, -0.494416, -0.544175, -0.586194] )
+
+        np.testing.assert_allclose(exc, ref, atol=1.e-5, rtol=0)
+
 if __name__ == '__main__':
     unittest.main()
